e01bb8f09792e71bba202f294d68e9afc69a9592
ryu <ryu@tohya.net>
2019-07-12 15:08:35 +0200

    check for text allocation error

--- lib/saturn_engine_core_text.h
+++ lib/saturn_engine_core_text.h
@@ -6,9 +6,9 @@ void saten_mrb_text_init(void)
             _saten_mrb_module, "Text", saten_mrb->object_class);
 
     mrb_define_class_method(saten_mrb, _saten_mrb_class_text,
-            "create", saten_mrb_text_create, MRB_ARGS_REQ(1));
+            "create", saten_mrb_text_create, MRB_ARGS_NONE());
     mrb_define_class_method(saten_mrb, _saten_mrb_class_text,
-            "free", saten_mrb_text_free, MRB_ARGS_REQ(1));
+            "free", saten_mrb_text_free, MRB_ARGS_NONE());
 
 }
 
@@ -17,7 +17,13 @@ mrb_value saten_mrb_text_create(mrb_state *mrb, mrb_value self)
     printf("called text create...\n");
     //saten_latest_text = saten_text_create(n); // list canned
     saten_latest_text = (saten_text*)saten_malloc(sizeof(saten_text));
-    return mrb_nil_value();
+    mrb_value ret;
+    if (saten_latest_text == NULL)
+        ret = mrb_fixnum_value((mrb_int)1);
+    else
+        ret = mrb_fixnum_value((mrb_int)0);
+    return ret;
+
 }
 
 mrb_value saten_mrb_text_free(mrb_state *mrb, mrb_value self)
--- script/saten_script_header.rb
+++ script/saten_script_header.rb
@@ -1,4 +1,5 @@
 module Saten
+
 =begin
   def Saten.printg(str)
     raw = false
@@ -8,21 +9,32 @@ module Saten
     end
   end
 =end
-  def Saten.set_text(id, str, x, y)
-    # creates glyph array representation of str
-    # id is glyph array to set, nil to create new array
-    # x, y top left position of glyph 0 on screen
-    if id.nil?
-      Text.create(nil)
-    else
-      # no use for now. might consider later if there are no better options to
-      # display numerical variables
-      # the idea is to change an existing text object to a new set of glyphs
-      Text.free(nil)
-    end
-    str = str.to_s
-    str.each_char do |c|
-      puts c
+
+  class Text
+    def Text.set(id, str, x, y)
+      # creates glyph array representation of str
+      # id is glyph array to set, nil to create new array
+      # x, y top left position of glyph 0 on screen
+      check = 0
+      if id.nil?
+        check = Text.create()
+        if check == 1
+          # failed to allocate memory for new text object
+        end
+      else
+        # no use for now. might consider later if there are no better options to
+        # display numerical variables
+        # the idea is to change an existing text object to a new set of glyphs
+        Text.free()
+      end
+      # don't append glyphs to nonexistant text...
+      if check == 0
+        str = str.to_s
+        str.each_char do |c|
+          puts c
+        end
+      end
     end
   end
+
 end
