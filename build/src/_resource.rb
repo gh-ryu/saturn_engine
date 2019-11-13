3e0b9eba83cd70d3d2e1fcf829891235c3ff1516
ryu <ryu@tohya.net>
2019-11-13 11:00:14 +0100

    make resource macro call more explicit

--- build/src/_resource.rb
+++ build/src/_resource.rb
@@ -3,13 +3,20 @@ class Resource
   RESOURCE_DIR    = "data/mrb/resource/"
   RESOURCE_HEADER = "include/saturn_engine/config/build/resource.h"
 
-  PREFIX = "#define SATEN_RESOURCE_"
+  PREFIX    = "SATEN_RESOURCE"
+  MACRO     = "#define #{PREFIX}(x) x"
+
+  MACRO_IMG = "#define #{PREFIX}SPR(x) saten_resource_img(x)"
+  MACRO_SFX = "#define #{PREFIX}SFX(x) saten_resource_sfx(x)"
+  MACRO_BGM = "#define #{PREFIX}BGM(x) saten_resource_bgm(x)"
+  MACRO_TEX = "#define #{PREFIX}TEX(x) saten_resource_text(x)"
 
   def initialize
     @cnt = Hash.new
     header = Util.fopen(RESOURCE_HEADER, 'w')
     header.puts "#ifndef SATURN_ENGINE_CONFIG_BUILD_RESOURCE"
     header.puts "#define SATURN_ENGINE_CONFIG_BUILD_RESOURCE"
+    header.puts MACRO
 
     # Scan each resource load file
     Dir.foreach(RESOURCE_DIR) do |x| 
@@ -43,7 +50,8 @@ class Resource
             m = line.match(/"(.*)"/)
             name = m[1].gsub("-", "_").gsub("/", "_").upcase
             name = name.slice(0..(name.index('.'))-1)
-            header.puts "#{PREFIX}SPRITE_#{@scene}_#{name}" \
+            #header.puts "#{PREFIX}SPRITE_#{@scene}_#{name}" \
+            header.puts "#define SPRITE_#{@scene}_#{name}" \
               " saten_resource_sprite(#{Config::SCENE_MANAGER}." \
               "#{@scene.swapcase},#{@cnt[@scene][:img]})"
             @cnt[@scene][:img] += 1
@@ -53,7 +61,8 @@ class Resource
             m = line.match(/"(.*)"/)
             name = m[1].gsub("-", "_").gsub("/", "_").upcase
             name = name.slice(0..(name.index('.'))-1)
-            header.puts "#{PREFIX}SFX_#{@scene}_#{name}" \
+            #header.puts "#{PREFIX}SFX_#{@scene}_#{name}" \
+            header.puts "#define SFX_#{@scene}_#{name}" \
               " saten_resource_sfx(#{Config::SCENE_MANAGER}." \
               "#{@scene.swapcase},#{@cnt[@scene][:sfx]})"
             @cnt[@scene][:sfx] += 1
@@ -63,14 +72,16 @@ class Resource
             m = line.match(/"(.*)"/)
             name = m[1].gsub("-", "_").gsub("/", "_").upcase
             name = name.slice(0..(name.index('.'))-1)
-            header.puts "#{PREFIX}BGM_#{@scene}_#{name}" \
+            #header.puts "#{PREFIX}BGM_#{@scene}_#{name}" \
+            header.puts "#define BGM_#{@scene}_#{name}" \
               " saten_resource_bgm(#{Config::SCENE_MANAGER}." \
               "#{@scene.swapcase},#{@cnt[@scene][:bgm]})"
             @cnt[@scene][:bgm] += 1
           end
           # Text
           if line.include?("Saten::Resource::Text.load")
-            header.puts "#{PREFIX}TEXT_#{@scene}_#{@cnt[@scene][:tex]}" \
+            #header.puts "#{PREFIX}TEXT_#{@scene}_#{@cnt[@scene][:tex]}" \
+            header.puts "#define TEXT_#{@scene}_#{@cnt[@scene][:tex]}" \
               " saten_resource_text(#{Config::SCENE_MANAGER}." \
               "#{@scene.swapcase},#{@cnt[@scene][:tex]})"
             @cnt[@scene][:tex] += 1
