8b1860a28691f3051a37b2f35be7f2f91701f626
ryu <ryu@tohya.net>
2019-12-05 15:33:22 +0100

    update scene builder

--- build/scene/scene.c
+++ build/scene/scene.c
@@ -2,8 +2,11 @@
 #include "helper/scene.h"
 #include "scene/{{low}}.h"
 
+scene_{{low}}_data *data;
+
 void scene_{{low}}_init(void)
 {
+    data = saten_scene_access_data(saten_scene_self());
 }
 
 void scene_{{low}}_update(bool c)
